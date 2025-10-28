483bff6332f45bd1382061c117b5f5f0b60d6974
GabrielCandido13 <gabriel.candido.sena@outlook.com>
2025-10-28 07:02:58 -0300

    Começando a trabalhar com JavaScript

---
+++ 2_OPERADORES/js/script.js
@@ -0,0 +1,24 @@
+// 1 -Number
+console.log(typeof(2));
+console.log(typeof(5.14));
+console.log(typeof(-127));
+
+// 2 - Ops. Aritméticas
+console.log(2 + 4);
+console.log(10 - 5);
+console.log(5 * 4);
+console.log(10 / 2);
+console.log(5 + 4 * 2);
+
+// 3 - Special numbers
+console.log(typeof(Infinity));
+console.log(typeof(-Infinity));
+console.log(12 * "asd");
+console.log(typeof(NaN));
+
+// 4 - Strings
+console.log("Um texto");
+console.log(`Mais um texto`);
+console.log(`13`);
+console.log(typeof("Um texto"));
+console.log(typeof(`Mais um texto`));
