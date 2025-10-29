a1da6ceb20141484e8f73aa3aa3ebf16b6a67892
GabrielCandido13 <gabriel.candido.sena@outlook.com>
2025-10-29 07:00:14 -0300

    Adicionando mais conteúdo sobre operadores em JavaScript

--- 2_OPERADORES/js/script.js
+++ 2_OPERADORES/js/script.js
@@ -22,3 +22,15 @@ console.log(`Mais um texto`);
 console.log(`13`);
 console.log(typeof("Um texto"));
 console.log(typeof(`Mais um texto`));
+
+// 5 - Símbolos especiais em Strings
+console.log("Testando a \nquebra de linha");
+console.log("Espaçamento\t de tab");
+
+// 6 - Concatenação
+console.log("Oi, " + "tudo" + " bem?");
+console.log(`Testando ` + `com ` + `crase!`);
+
+// 7 - Template Strings
+console.log(`A soma de 2 + 2 é: ${2 + 2}`);
+console.log(`Podemos executar qualquer coisa aqui ${console.log("teste")}`);
