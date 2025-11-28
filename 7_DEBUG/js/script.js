3923112f7594daf38266f222f44aa58b62dc6772
GabrielCandido13 <gabriel.candido.sena@outlook.com>
2025-11-28 17:12:14 -0300

    Conclusão Debug e Tratamento de Erros

---
+++ 7_DEBUG/js/script.js
@@ -0,0 +1,95 @@
+// 1 - Scrict
+"use strict";
+
+// opa = "teste";
+
+// 2 - console.log
+let a = 1;
+let b = 2;
+
+if(a === 1) {
+    a = b + 2;
+}
+
+console.log(a);
+
+for(let i = 0; i < b; i++) {
+    a = a + 2;
+    console.log(a);
+}
+
+// 3 - Debugger
+let c = 1;
+let d = 2;
+
+if(c === 1) {
+    c = d + 2;
+};
+
+// debugger;
+
+for(let i = 0; i < d; i++) {
+    c = c + 2;
+};
+
+console.log("Executou o loop");
+
+// debugger;
+
+if(c > 25) {
+    c = 25;
+};
+
+// 4 - Tratamentos de dados
+function checkNumber(n) {
+    const result = Number(n);
+
+    if(Number.isNaN(result)) {
+        console.log("Valor incorreto");
+        return;
+    };
+
+    console.log("Valor correto!");
+    return result;
+}
+
+checkNumber(5);
+checkNumber("a");
+
+// 5 - Expections
+let x = 10;
+/* if(x != 11) {
+    throw new Error("O valor de X não pode ser diferente de 11!");
+}; */
+
+// 6 - Try catch
+try {
+    const soma = x + y;
+} catch(error) {
+    console.log(`Erro no programa: ${error}`)
+};
+
+// 7 - Finally
+try {
+    const value = checkNumber("asd");
+
+    if(!value) {
+        throw new Error("Valores inválidos!")
+    }
+} catch(error) {
+    console.log(`Opa, aconteceu um problema: ${error}`);
+} finally {
+    console.log("O código foi executado!");
+}
+
+// 8 - Assertion
+function checkArray(arr) {
+    if(arr.length === 0) {
+        throw new Error("O Array precisa ter elementos!");
+    } else {
+        console.log(`O array tem ${arr.length} elementos.`);
+    }
+}
+
+// checkArray([]);
+checkArray([1, 2, 3]);
